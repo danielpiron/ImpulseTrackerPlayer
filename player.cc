065c2c95903b95fc0a6cce5ab84e36ed9f297e35
Daniel Piron <dannypiron@gmail.com>
2018-06-17 23:24:40 -0400

    Add experimental 'player' executable

---
+++ player.cc
@@ -0,0 +1,93 @@
+#include <array>
+#include <cstdint>
+#include <iostream>
+
+struct PatternEntry {
+    class Note {
+        static const uint8_t empty = 253;
+        static const uint8_t cut = 254;
+        static const uint8_t off = 255;
+
+    public:
+        bool is_empty() const { return _index == empty; }
+        bool is_cut() const { return _index == cut; }
+        bool is_off() const { return _index == off; }
+        int octave() const { return _index / 12; }
+        int semitone() const { return _index % 12; }
+        int period() const
+        {
+            static const short periods[] = {
+                1712,
+                1616,
+                1524,
+                1440,
+                1356,
+                1280,
+                1208,
+                1140,
+                1076,
+                1016,
+                960,
+                907
+            };
+            return 16 * periods[semitone()] / octave();
+        }
+        operator int() const { return _index; }
+        Note() = default;
+        explicit Note(const uint8_t i)
+            : _index(i)
+        {
+        }
+
+    private:
+        uint8_t _index = empty;
+    };
+    class Inst {
+    public:
+        static const uint8_t empty = 255;
+        Inst() = default;
+        explicit Inst(const uint8_t i)
+            : _index(i)
+        {
+        }
+
+    private:
+        uint8_t _index = empty;
+    };
+    class Command {
+    public:
+        enum class Type : uint8_t {
+            none,
+            set_speed,
+            set_tempo,
+            set_volume,
+            set_panning
+        };
+        Command() = default;
+        explicit Command(Type t, uint8_t v)
+            : _type(t)
+            , _value(v)
+        {
+        }
+
+    private:
+        Type _type = Type::none;
+        uint8_t _value = 0;
+    };
+    using Comms = std::array<Command, 2>;
+
+    Note note;
+    Inst inst;
+    Comms comms;
+};
+
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    PatternEntry note;
+    std::cin >> argc;
+
+    return 0;
+}
